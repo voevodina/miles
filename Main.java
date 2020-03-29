4ad7edc2d016970cace8b1187f98c8292a145bec
voevodina <voevodina_lyubov@mail.ru>
2020-03-29 19:48:25 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float price = 21054;
+        float miles = price / 20;
+        //за каждые 20 рублей, потраченные на билет, начисляется 1 миля
+        System.out.println(miles);
+    }
+}
